2c31c0b7ac4f5d5388713cc7ee8d609944a79f98
Oleg Krokhinov <Oleg.krokhinov@gmail.com>
2021-04-25 00:22:16 +0300

    Use mongoDB instead users.json

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const users = require('./users')
 
+const MongoClient = require("mongodb").MongoClient;
+const mongoClient = new MongoClient("mongodb://localhost:27017/", { useUnifiedTopology: true });
+let dbClient;
+
+
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true }))
 
@@ -11,8 +16,17 @@ app.use(function (req, res, next) {
     res.send(`The route ${req.path} can't be found`);
   });
 
-app.listen(3000, () => {
-    console.log(`Waiting for connections ...`)
-  })
 
+mongoClient.connect(function(err, client){
+  if(err) return console.log(err);
+  dbClient = client;
+  app.locals.db = client.db("NodeSrv");
+  app.listen(3000, function(){
+    console.log(`Waiting for connections ...`);
+  });
+});
 
+process.on("SIGINT", () => {
+  dbClient.close();
+  process.exit();
+});
--- users.js
+++ users.js
@@ -1,66 +1,56 @@
 const express =  require('express');
 const router = express.Router();
-const pathToUsersJson = './users.json';
-
-function loadData(pathToJson){
-    const fs = require('fs');
-    const data = fs.readFileSync(pathToJson);
-    return JSON.parse(data);
-};
-
-function saveData(data, pathToJson){
-    const fs = require('fs');
-    fs.writeFileSync(pathToJson, JSON.stringify(data));
-};
 
 
 router.get('/', (req, res, next) => {
-    res.send(loadData(pathToUsersJson));                  
+    
+    let usersCollection = req.app.locals.db.collection('users');   
+    usersCollection.find({}).toArray(function (err, users){
+        if(err) return console.log(err);
+        res.send(users);
+    });
+
 });
 
 router.get('/:User', (req, res, next) => {
     
-    let User = loadData(pathToUsersJson).find(item => item.name == req.params.User);
-    User ? res.send(User) : res.send(`User ${req.params.User} not found.`);                  
+    let usersCollection = req.app.locals.db.collection('users');   
+    usersCollection.findOne({name : req.params.User}, (err, users) => {
+        if(err) return console.log(err);
+        res.send(users);
+    });
+    
 });
 
 router.put('/update', (req, res, next) => {
     
-    let users = loadData(pathToUsersJson);
-    let User = users.find(item => item.name == req.body.name);
-    if (User) {
-        User.age = req.body.age;
-        saveData(users, pathToUsersJson);
-        res.send(`User ${req.body.name} changed.`);
-    } else {
-        res.send(`User ${req.body.age} not found.`); 
-    };
+    let usersCollection = req.app.locals.db.collection('users');   
+    usersCollection.findOneAndUpdate({name : req.body.name}, { $set: {age: req.body.age}}, (err, users) => {
+        if(err) return console.log(err);
+        res.send(users.value);
+    });
                      
 });
 
 router.post('/add', (req, res, next) => {
-    
-    let users = loadData(pathToUsersJson);
-    if (users.findIndex(item => item.name == req.body.name) == -1) {
+
         newUser = {name: req.body.name, age: req.body.age}; 
-        users.push(newUser);
-        saveData(users, pathToUsersJson);
-        res.send(`User ${req.body.name} added`);
-    } else {
-        res.send(`User ${req.body.name} already exists.`); 
-    };                  
+        
+        let usersCollection = req.app.locals.db.collection('users');   
+        usersCollection.insertOne(newUser, (err, users) => {
+            if(err) return console.log(err);
+            res.send(users.ops);
+        });               
 
 });
 
 router.delete('/delete', (req, res, next) => {
     
-    let users = loadData(pathToUsersJson);
-    if (users.splice(users.findIndex(item => item.name == req.body.name))) {
-        saveData(users, pathToUsersJson);
-        res.send(`User ${req.body.name} deleted.`);
-    } else {
-        res.send(`User ${req.body.name} not found.`); 
-    }; 
+    let usersCollection = req.app.locals.db.collection('users');   
+    usersCollection.findOneAndDelete({name : req.body.name}, (err,users) => {
+        if(err) return console.log(err);
+        res.send(users.value);
+    });      
 
 });
 
