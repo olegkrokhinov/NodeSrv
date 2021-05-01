17c92c7199c4783359deb16e1e03e83eacf8d93a
Oleg Krokhinov <Oleg.krokhinov@gmail.com>
2021-05-01 04:04:59 +0300

    Added API for friend request: send(add) request, approve, delete

--- userModel.js
+++ userModel.js
@@ -1,18 +1,111 @@
 const mongoose = require('mongoose');
 
+const friendSchema = new mongoose.Schema({
+  friendId : mongoose.SchemaTypes.ObjectId,
+  approved : {
+    type: Boolean,
+    default : false
+  }
+});
 
 const userSchema = new mongoose.Schema({
   name: String,
-  age: Number
+  age: Number,
+  friends: [friendSchema],  
 });
 
-userSchema.methods.aboutMe = function () {
-  //this.name;
-  //this.age;
-  console.log('user method aboutMe');
-}
 
-mongoose.model('User', userSchema, 'users');
+userSchema.methods.friendRequestAdd = function(friendId, cb) {
+  
+  let friend =  this.friends.find(item => {(item.friendId == friendId)});
+  if (!friend) {
+    newFriend = new (mongoose.model('Friend', friendSchema))({friendId : friendId});
+    this.friends.push(newFriend);
+    this.save();
+    cb('', newFriend);
+  } else {
+    cb('', friend);
+  }
+};
+
+userSchema.methods.friendRequestApprove = function(friendId, cb) {
+ 
+  let friend =  this.friends.find(item => {
+    return (item.friendId == friendId);
+  });
+  if (friend) {
+    friend.approved = true; // approved
+     this.save();
+     cb('', friend);
+  } else {
+    cb('', {})
+  };
+};
+
+userSchema.methods.friendDelete = function(friendId, cb) {
+  ;
+  let friendArrIndex= this.friends.findIndex(item => item.friendId == friendId);
+  //let friend = this.friends[friendArrIndex];
+  
+  if (friendArrIndex >= 0) {
+    this.friends.splice(friendArrIndex, 1);
+    this.save(); 
+    cb('', {}); 
+  } else {
+    cb('', {});
+  };
+};
+
+userSchema.methods.aboutMe = function () {
+  console.log('user method aboutMe');
+};
+
+userSchema.statics.findUser_And_AddFriendRequest = function(userId, friendId, cb) {
+  
+  this.findById( userId, (err, user)=>{
+    if(err) return console.log(err);
+    user.friendRequestAdd(friendId, (err, friend)=>{
+      if(err) return console.log(err);
+      cb('', friend);
+    });
+  });
+
+  //doc.set('socialHandles.stackOverflow', 'vkarpov15'); 
+  //doc.get('socialHandles.stackOverflow')); 
+
+};
+
+userSchema.statics.findUser_And_ApproveFriend = function(userId, friendId, cb) {
+
+  this.findById( userId, (err, user)=>{
+    if(err) return console.log(err);
+    user.friendRequestApprove(friendId, (err, friend)=>{
+    if(err) return console.log(err);
+    cb('', friend);
+  });
+
+  });
+};
+
+userSchema.statics.findUser_And_DeleteFriend = function(userId, friendId, cb) {
+
+  this.findById( userId, (err, user)=>{
+    if(err) return console.log(err);
+    
+   user.friendDelete(friendId, (err, friend)=>{
+    if(err) return console.log(err);
+    cb('', friend);
+  });
+
+  });
+};
+
+
+
+
+const userModel = mongoose.model('User', userSchema, 'users');
+
+
 
 
 //module.exports = UserModel;
--- userRouter.js
+++ userRouter.js
@@ -23,16 +23,32 @@ router.get('/:User', (req, res, next) => {
     
 });
 
-router.put('/update', (req, res, next) => {
+router.get('/:User/friends', (req, res, next) => {
+    
+    userModel.findOne({name : req.params.User}, (err, user) => {
+        if(err) return console.log(err);
+        res.send(user.friends);
+    });  
+
+});
+
+router.put('/updateUser', (req, res, next) => {
     
     userModel.findOneAndUpdate({name : req.body.name}, {age: req.body.age}, (err, user) => {
         if(err) return console.log(err);
         res.send(user ? user.value : `User ${req.body.name} not found.`);
-    });
-      //`useFindAndModify`                
+    });             
 });
 
-router.post('/add', (req, res, next) => {
+router.post('/approveFriend', (req, res, next) => {
+    userModel.findUser_And_ApproveFriend(req.body.userId, req.body.friendId, (err, friend)=>{
+        if(err) return console.log(err);
+        res.send(friend); 
+    });
+});
+
+
+router.post('/addUser', (req, res, next) => {
 
     let user = new userModel({name: req.body.name,age: req.body.age});
     user.save((err, user) => {
@@ -42,7 +58,21 @@ router.post('/add', (req, res, next) => {
 
 });
 
-router.delete('/delete', (req, res, next) => {
+router.post('/addFriendRequest', (req, res, next) => {
+    userModel.findUser_And_AddFriendRequest(req.body.userId, req.body.friendId, (err, friend)=>{
+        if(err) return console.log(err);
+        res.send(friend); 
+    });
+});
+
+router.delete('/deleteFriend', (req, res, next) => {
+    userModel.findUser_And_DeleteFriend(req.body.userId, req.body.friendId, (err, friend) =>{
+        if(err) return console.log(err);
+        res.send(friend); 
+    });
+});
+
+router.delete('/deleteUser', (req, res, next) => {
     
     userModel.findOneAndDelete({name : req.body.name}, (err,user) => {
         if(err) return console.log(err);
