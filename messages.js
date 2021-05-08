6de38203f3bba3d67c734065daef199c55ead068
Oleg Krokhinov <Oleg.krokhinov@gmail.com>
2021-05-08 11:20:36 +0300

    Added controller and messages for user model

---
+++ messages.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+    error: {
+        FRІEND_ALREADY_REQUESTED : 'Friend (friend request) already requested.',
+        FRІEND_ALREADY_APPROVED : 'Friend (friend request) already approved.',
+        FRІEND_REJECTED : 'Friend (friend request) rejected.',
+        FRІEND_ALREADY_REJECTED : 'Friend (friend request) alredy rejected.',
+        FRIEND_NOT_FOUND :'Friend (friend request) not found.',
+        USER_NOT_FOUND : 'User not found.',
+        FAILED_TO_SAVE_USER : 'Failed to save user to database',
+    } 
+    
+}
--- userRouter.js
+++ userRouter.js
@@ -2,68 +2,19 @@ const express =  require('express');
 const router = express.Router();
 
 const userModel = require('./userModel');
+userController = require('./userController')
 
+router.get('/', userController.getUsers);
 
-router.get('/', (req, res, next) => {
-  userModel.find().exec()
-    .catch((error) => res.send(error.message))   
-    .then((user) => {res.send(user)})        
-});
+router.get('/:user', userController.getUser);
+router.put('/:user', userController.updateUser);
+router.post('/', userController.addUser);
+router.delete('/', userController.deleteUser);
 
-router.get('/:user', (req, res, next) => {
-   userModel.find({_id : req.body.userId}).exec()
-     .catch((error) => res.send(error.message))   
-     .then((user) => {res.send(user)});
-});
-
-router.put('/:user', (req, res, next) => {
-  userModel.findOneAndUpdate({_id: req.body.userId}, {name : req.body.name, age: req.body.age}).exec()
-    .catch((error) => res.send(error.message))
-    .then((user) => {res.send(user)});
-});
-
-router.post('/', (req, res, next) => {
-  (new userModel({name: req.body.name, age: req.body.age})).save()
-    .catch((error) => res.send(error.message))     
-    .then((user) => {res.send(user)});
-});
-
-router.delete('/', (req, res, next) => {
-  userModel.findOneAndDelete({_id: req.body.userId}).exec()
-    .then((user) => {res.send(user)})
-    .catch((error) => res.send(error.message)); 
-});
-
-router.get('/:user/friends', (req, res, next) => {
-  userModel.findOne({_id : req.body.userId}).exec()
-    .then((user) => {res.send(user.friends)})     //sends friends
-    .catch((error) => res.send(error.message));         //sends Error object  
-});  
-
-
-router.post('/:User/friends', (req, res, next) => {
-  userModel.findUserAndDoAnActionWithFriend(req.body.userId, req.body.friendId, 'addFriendRequest')
-    .then((friend) => res.send(friend))   //sends friend object
-    .catch((error)=> res.send(error.message)); //sends Error object
-});
-
-router.put('/:user/friends/:friend', (req, res, next) => {
-  userModel.findUserAndDoAnActionWithFriend(req.body.userId, req.body.friendId, 'approveFriendRequest')
-    .then((friend) => res.send(friend))   //sends friend object
-    .catch((error) => res.send(error.message)); //sends Error object
-});
-
-router.put('/:user/friends/:friend', (req, res, next) => {
-  userModel.findUserAndDoAnActionWithFriend(req.body.userId, req.body.friendId, 'rejectFriendRequest')
-    .then((friend) => res.send(friend))   //sends friend object
-    .catch((error) => res.send(error.message)); //sends Error object
-});
-
-router.delete('/:user/friends', (req, res, next) => {
-  userModel.findUserAndDoAnActionWithFriend(req.body.userId, req.body.friendId, 'deleteFriendRequest')
-    .then((friend) => res.send(friend))   //sends friend object
-    .catch((error) => res.send(error.message)); //sends Error object
-});
-
+router.get('/:user/friends', userController.getUserFriends);  
+router.post('/:User/friends', userController.addFriendRequest);
+router.put('/:user/friends/:friend', userController.approveFriendRequest);
+router.delete('/:user/friends/:friend', userController.rejectFriendRequest);
+router.delete('/:user/friends', userController.deleteFriendRequest);
 
 module.exports = router;
