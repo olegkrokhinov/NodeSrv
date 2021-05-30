0458d0868a6c7f25342edd64987fd231a8bce9f8
olegkrokhinov <oleg.krokhinov@gmail.com>
2021-05-30 23:45:46 +0300

    used appConfig.js for credentials

--- app.js
+++ app.js
@@ -2,21 +2,15 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
-const uri = 'mongodb+srv://NodeSrvUser:mVaDEtZKO3SOJ5nl@cluster0.arqzx.mongodb.net/NodeSrvDB?retryWrites=true&w=majority';
-const port = 4000;
+const { port, dbUri } = require('./appConfig')
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.all('*', (req, res, next) => {
-  res.header("Access-Control-Allow-Headers", "access-control-allow-origin, content-type")
-  res.header("Access-Control-Allow-Origin", "*");
-  next();
-});
-
 require('./appRoutes')(app);
 
-mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(dbUri, {useNewUrlParser: true, useUnifiedTopology: true})
 .then(()=> {
   app.listen(port, function(){
     console.log(`Waiting for connections at port ${port}...`);
--- appRoutes.js
+++ appRoutes.js
@@ -1,9 +1,18 @@
 
 module.exports =  function loadRoutesToApp(app) {
-    app.use('/users', require('./api/user'));
+  app.all('*', (req, res, next) => {
+    res.header("Access-Control-Allow-Headers", "access-control-allow-origin, content-type")
+    res.header("Access-Control-Allow-Origin", "*");
+    next();
+  });
+    
+  app.use('/users', require('./api/user'));
     app.use('/auth', require('./api/auth'));    
     
     app.use((req, res, next) => {
       res.send(`The route ${req.path} can't be found`);
     });
+
+    
+
 }
